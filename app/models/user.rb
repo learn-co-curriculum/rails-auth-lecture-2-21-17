6c1403a22ba7571a9bf55e53587d8c2463cc0e29
Luke Ghenco <lukeghenco@gmail.com>
2017-02-21 09:49:41 -0500

    set up bcrypt with User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password # hashing password, checking presence of password with AR validations, authenticate user's password ( user.authenticate(password) ) user.password_digest = "43234234i2u4$$$q32io1o$,rdhakjlahrkjlahrajrhrale" params[:password] = "password"
+
+    validates :username, presence: true 
+    validates :username, uniqueness: true 
 
 end
